8580c3f31f2e665bc3b4c3977baedd3b8cd0062e
Ahmed Raza <aimen69khan@gmail.com>
2026-06-12 16:57:41 +0500

    Calculator Done

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
-function input(){
-    let div = document.getElementById('divBtn');
-    let btn = div.getElementsByTagName('button');
-    let input1 = document.getElementById('inputField');
+let display = document.getElementById('display');
 
-    for(var i=0; i<btn.length; i++){
-        btn[i].addEventListener('click', function(){
-            input1.value += this.textContent;
-        });
+const appendValue = value => display.value += value;
+const clearDisplay = () => display.value = "";
+const deleteLast = () => display.value = display.value.slice(0, -1);
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
     }
 }
-input()
-
-function addition(a,b){
-    return a + b;
-}
-function subtraction(a,b){
-    return a - b;
-}
-function multiplation(a,b){
-    return a * b;
-}
-function divided(a,b){
-    return a / b;
-}
-// function 
